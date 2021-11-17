d358d32e01c23a3c0729823cdd069102753f02d7
JaimeGutierrezM <jaime.gutierrez@ucsp.edu.pe>
2021-11-17 16:24:28 -0500

    Creación Voucher_Clientes

---
+++ voucher.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <math.h> //para formulas matemáticas
+#include <conio.h> //gets()
+#include <cstring> //strcpy strcmp
+#include <String.h> //string 
---
+++ voucher.h
@@ -0,0 +1,15 @@
+#include <cstring> //para strcpy
+#include <conio.h> //gets()
+
+
+class Voucher_Clientes {
+
+//Atributos
+
+
+//Constructor
+
+
+//Metodos
+
+}
